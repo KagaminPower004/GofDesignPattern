28147ac5eb0a11ef682f5eb5e0db69b4b08101a1
nezucchi8078 <nezucchi8078@protonmail.com>
2021-08-31 07:43:27 +0900

    2021/8/31 Singletonパターンのソースを一部変更

--- 23_Singleton/Singleton01/Beauty.java
+++ 23_Singleton/Singleton01/Beauty.java
@@ -13,19 +13,19 @@ class Beauty{
 
         System.out.println("～20代の頃のピュアさん～");
         Pure pure20 = Pure.getInstance();
-        System.out.println("ピュアさんって言うんだ。初めましてだね!");
+        System.out.println("変な人だなぁ。いつも挙動不審だ。。。");
 
         System.out.println("\n～30代の頃のピュアさん～");
         Pure pure30 = Pure.getInstance();
 
-        if( pure20 == pure30 ){System.out.println("30代のピュアさん、20代の頃と変わってないね!");}
-        else{System.out.println("30代になって、お酒が弱い人に変わっちゃったね。。。");}
+        if( pure20 == pure30 ){System.out.println("変な人だなぁ。いつも挙動不審だ。。。");}
+        else{System.out.println("30代らしく、責任感やリーダーシップのある人に変わったね♪♪");}
 
         System.out.println("\n～40代の頃のピュアさん～");
         Pure pure40 = Pure.getInstance();
 
-        if( pure20 == pure40 ){System.out.println("40代のピュアさん、あの時と変わってないね!");}
-        else{System.out.println("40代になって、髪の毛がところどころ白くなっちゃったね。。。");}
+        if( pure20 == pure40 ){System.out.println("変な人だなぁ。いつも挙動不審だ。。。");}
+        else{System.out.println("40代になって、立派な役職もついて貫禄がついたね♪♪♪");}
 
     }
 }
